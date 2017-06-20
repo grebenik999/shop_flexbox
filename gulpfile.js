1971f4f8204f93c5362e2dde542071e8aad087de
Гребеник <grebenik999@mail.ru>
2017-06-20 19:13:32 +0300

    format styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var autoprefixer = require('gulp-autoprefixer');
 var notify = require('gulp-notify');
-var cleanCSS = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var cssbeautify = require('gulp-cssbeautify');
+var gcmq = require('gulp-group-css-media-queries');
 
 
 
-gulp.task('browser-sync',['sass'], function() {
+gulp.task('browser-sync',['sass','css'], function() {
     browserSync.init({
         server: {
             baseDir: "./app"
@@ -16,12 +18,23 @@ gulp.task('browser-sync',['sass'], function() {
     });
 });
 
+gulp.task('css', function() {
+    return gulp.src('./app/css/*.css')
+        .pipe(cssbeautify({
+            indent: ' ',
+            openbrace: 'end-of-line',
+            autosemicolon: true
+        }))
+        .pipe(gulp.dest('./app/css'));
+});
+
 gulp.task('sass', function(){
     gulp.src('scss/*.scss')
         .pipe(sass({includePaths: require('bourbon').includePaths})
         .on('error', notify.onError()))
         .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(csscomb())
+        .pipe(gcmq())
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream());
 });
